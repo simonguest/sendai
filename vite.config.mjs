40b94082b72f1ffa0b9f28907af40226c55ac604
Simon Guest <me@simonguest.com>
2025-05-06 06:56:18 -0700

    Changed vite config to relative base

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,6 +7,7 @@ import { resolve } from "path";
 import ignorePyodidePlugin from "./vite-plugin-ignore-pyodide";
 
 export default defineConfig({
+  base: "./",
   plugins: [
     viteStaticCopy({
       targets: [
