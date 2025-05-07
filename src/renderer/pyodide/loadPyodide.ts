71963ba823c9728c9bc6ed492302bdeab01c8b73
Simon Guest <me@simonguest.com>
2025-05-06 19:55:53 -0700

    Updated relative URLs for dev and build

--- src/renderer/pyodide/loadPyodide.ts
+++ src/renderer/pyodide/loadPyodide.ts
@@ -1,4 +1,12 @@
-export async function loadPyodideModule() {
+const getURL = () => {
   // @ts-ignore
-  return await import(new URL(/* @vite-ignore */`/assets/pyodide.mjs`, import.meta.url));
+  if (import.meta.env.DEV){
+    return new URL(/* @vite-ignore */`/assets/pyodide.mjs`, import.meta.url)
+  } else {
+    return new URL(new URL(/* @vite-ignore */`./pyodide.mjs`, import.meta.url));
+  }
+}
+
+export async function loadPyodideModule() {
+  return await import(getURL().toString());
 }
