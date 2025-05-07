55fcb0c026f75bb06a7a1066384a55db6111b4e4
Simon Guest <me@simonguest.com>
2025-05-06 20:31:59 -0700

    Added rollup back and pyodide.mjs.map

--- vite.config.mjs
+++ vite.config.mjs
@@ -33,15 +33,15 @@ export default defineConfig({
   ],
   publicDir: false,
   build: {
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks(id) {
-    //       if (id.includes("node_modules")) {
-    //         return id.toString().split("node_modules/")[1].split("/")[0].toString();
-    //       }
-    //     },
-    //   },
-    // },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
   },
   resolve: {
     alias: {
