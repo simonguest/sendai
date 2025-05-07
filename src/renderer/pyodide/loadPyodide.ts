a356c1205e1dc4d4f2a45e750e43290236aeae4a
Simon Guest <me@simonguest.com>
2025-05-06 20:14:46 -0700

    Trying to make call stack smaller

--- src/renderer/pyodide/loadPyodide.ts
+++ src/renderer/pyodide/loadPyodide.ts
@@ -1,12 +1,8 @@
-const getURL = () => {
-  // @ts-ignore
-  if (import.meta.env.DEV){
-    return new URL(/* @vite-ignore */`/assets/pyodide.mjs`, import.meta.url)
-  } else {
-    return new URL(new URL(/* @vite-ignore */`./pyodide.mjs`, import.meta.url));
-  }
-}
-
 export async function loadPyodideModule() {
-  return await import(getURL().toString());
+  return await import(
+    // @ts-ignore
+    import.meta.env.DEV
+      ? new URL(/* @vite-ignore */ `/assets/pyodide.mjs`, import.meta.url).toString()
+      : new URL(/* @vite-ignore */ `./pyodide.mjs`, import.meta.url).toString()
+  );
 }
