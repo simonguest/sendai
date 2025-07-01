21994bc0be1eeaf84e090ad760fe216c13ec8dad
Simon Guest <me@simonguest.com>
2025-06-30 17:33:48 -0700

    Basic pygame functionality working

--- renderer/src/pyodide/overrides/implementOverride.ts
+++ renderer/src/pyodide/overrides/implementOverride.ts
@@ -1,6 +1,12 @@
-export const overrides = ["matplotlib"]
+export const overrides = ["matplotlib", "pygame-ce"]
 
 export const implementOverride = async (pyodide: any, module: string) => {
+  //TODO: Need to find some way of linking additional packages to overrides
+  switch (module) {
+    case "pygame-ce":
+      await pyodide.loadPackage("pillow");
+      break;
+  }
   const response = await fetch(new URL(`./${module}.py`, import.meta.url));
   const code = await response.text();
   await pyodide.runPythonAsync(code);
